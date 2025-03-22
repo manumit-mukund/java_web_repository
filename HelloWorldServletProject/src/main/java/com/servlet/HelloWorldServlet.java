62474f72c34980450730c512879bf2f41ab50422
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-03-22 12:55:12 +0530

    First Commit...

---
+++ HelloWorldServletProject/src/main/java/com/servlet/HelloWorldServlet.java
@@ -0,0 +1,44 @@
+package com.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/HelloWorldServlet")
+public class HelloWorldServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public HelloWorldServlet() {
+
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+
+		out.print("<html><body>");
+		out.print("<h2>Hello World Servlet</h2>");
+		out.print("</body></html>");
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+
+	}
+
+}
---
+++ java_app_jenkins/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World 123!</h2>
+ <a href="test.jsp">Go to the test page</a>
+</body>
+</html>
